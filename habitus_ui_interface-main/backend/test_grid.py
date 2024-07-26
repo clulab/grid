b83a5f49426f1ec7674653f6f25870ff2cf34b57
Keith Alcock <github@keithalcock.com>
2024-07-26 15:19:15 -0700

    Handle unknown words

--- habitus_ui_interface-main/backend/test_grid.py
+++ habitus_ui_interface-main/backend/test_grid.py
@@ -1,5 +1,3 @@
-# import unittest
-
 from .backend import Backend
 from .grid import Grid
 
@@ -27,25 +25,25 @@ class GridConfig2():
 
 class GridTest(): # (unittest.TestCase):
 	def __init__(self):
-		self.gridConfig = GridConfig1()
-		self.backend: Backend = Backend(self.gridConfig.path)
+		pass
 
-	def setUp(self):
+	def write(self, gridConfig):
+		backend: Backend = Backend(gridConfig.path)
 		# This below should produce from a filepath ending with {wikipedia_article}/
 		# path/cleaned_{wikipedia_article}.csv
 		# path/cleaned_{wikipedia_article}_doc_distances_lem.npy
 		# path/cleaned_{wikipedia_article}_doc_vecs_lem.json
 		# path/{wikipedia_article}.csv
 		# path/{wikipedia_article}_row_labels.csv
-		self.backend.process_supercorpus(self.gridConfig.supercorpus_filepath)
+		backend.process_supercorpus(gridConfig.supercorpus_filepath)
 
 		# This below only works after the files have been generated.
-		self.backend.set_superfiles(self.gridConfig.supercorpus_filename, self.gridConfig.row_filename)
-		self.grid: Grid = self.backend.get_grid( \
-			self.gridConfig.k, \
-			self.gridConfig.anchor, \
-			self.gridConfig.grid_filename, \
-			self.gridConfig.clustering_algorithm \
+		backend.set_superfiles(gridConfig.supercorpus_filename, gridConfig.row_filename)
+		grid: Grid = backend.get_grid( \
+			gridConfig.k, \
+			gridConfig.anchor, \
+			gridConfig.grid_filename, \
+			gridConfig.clustering_algorithm \
 		)
 		# This below will create the files given {anchor}
 		# path/{wikipedia_article}_{anchor}_cells.csv
@@ -53,20 +51,28 @@ class GridTest(): # (unittest.TestCase):
 		# path/{wikipedia_article}_{anchor}_specs.csv
 		# path/{wikipedia_article}_{anchor}_tokens.csv
 		# path/{wikipedia_article}_{anchor}_vectors.csv
-		self.grid.dump()
+		grid.dump()
 
 		# The above 5 files should be turned into a single file for export or import.
 		# If need be, they could be separate but zipped.
 
-	def reuse(self):
-		self.backend.set_superfiles(self.gridConfig.supercorpus_filename, self.gridConfig.row_filename)
-
-
-	def test_delete_document(self):
-		self.assertEqual(1, 1)
+	def read(self, gridConfig):
+		backend: Backend = Backend(gridConfig.path)
+		backend.set_superfiles(gridConfig.supercorpus_filename, gridConfig.row_filename)
+		grid: Grid = backend.get_grid( \
+			gridConfig.k, \
+			gridConfig.anchor, \
+			gridConfig.grid_filename, \
+			gridConfig.clustering_algorithm \
+		)
+		grid.dump()
 
 if __name__ == '__main__':
-	# unittest.main()
 	gridTest = GridTest()
-	gridTest.setUp()
-	gridTest.reuse()
+
+	gridConfig = GridConfig1()
+	# gridTest.write(gridConfig)
+	# gridTest.read(gridConfig)
+
+	gridConfig = GridConfig2()
+	gridTest.read(gridConfig)
